76423b19bc4be017f26c25a998670661caa56e8c
Jeferson Rodrigues <Jeferson.Rodrigues-ext@ab-Inbev.com>
2023-01-04 10:34:59 -0300

    Add Java Spring Application

---
+++ src/main/java/com/example/techvalidation/TechValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.techvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechValidationApplication.class, args);
+	}
+
+}
